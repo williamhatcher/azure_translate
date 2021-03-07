e1f06b8121d4072fe2bad1c4c7bf10c93cd2e90b
William 'psyFi' Hatcher <william@hatcher.work>
2021-03-07 10:00:33 -0600

    Updated docs & bumped version Added CHANGELOG

--- src/azure_translate.nim
+++ src/azure_translate.nim
@@ -1,4 +1,18 @@
-import httpclient, tables, json, options, strutils, uri
+## Microsoft Azure Translate API wrapper for Nim
+## 
+## This implements all v3 methods of the Azure Cognitive Services Translate API in both sync and async form.
+##
+## Example Usage
+## =====
+## 
+## .. code-block::Nim
+##  let client = newHttpClient()
+##  let key = "YOUR_AZURE_API_KEY"
+##  let input_text = @["hello world"]
+##  let to_langs = @["es"]
+##  echo translate(client, key, input_text, to_langs) 
+##   
+import httpclient, tables, json, options, strutils, uri, asyncdispatch
 import nuuid
 
 const 
@@ -9,33 +23,31 @@ type
     AzureTranslateError* = object of CatchableError
         ## Raised when the API returns an error. See `msg` as well
         code*: int
-    TextQuery = openArray[string]
+    TextQuery = seq[string]
     DictExQuery* = tuple[text, translation: string]  ## Type alias for `dictionay_example` endpoint parameter
     Scope* = enum
         ## Scope for `languages` endpoint
         azTranslation, azTransliteration, azDictionary
     TextType* {.pure.} = enum
-        ## for `translate` endpoint
+        ## Input text type for `translate` endpoint
         plain, html
     ProfanityAction* {.pure.} = enum
-        ## for `translate` endpoint
+        ## Profanity handling for `translate` endpoint
         noAction, marked, deleted
     ProfanityMarker* {.pure.} = enum
-        ## for `translate` endpoint
+        ## Profanity handling for `translate` endpoint
         asterisk, tag
 
     # Sub-objects
-    BaseLanguage* = object of RootObj
-        name*, nativeName*: string
-
     ScriptLanguage* = object
         code*, name*, nativeName*, dir*: string
         toScripts*: Option[seq[ScriptLanguage]]
 
-    TranslationLanguage* = object of BaseLanguage
-        dir*: string
+    TranslationLanguage* = object
+        name*, nativeName*, dir*: string
 
-    TransliterationLanguage* = object of BaseLanguage
+    TransliterationLanguage* = object
+        name*, nativeName*: string
         scripts*: seq[ScriptLanguage]
 
     DictionaryTranslation* = object
@@ -82,9 +94,10 @@ type
         translation*: Option[Table[string, TranslationLanguage]]
         transliteration*: Option[Table[string, TransliterationLanguage]]
         dictionary*: Option[Table[string, DictionaryLanguage]]
-
+ 
     Translation* = object 
-        detectedLanguage*: Option[DetectedLanguage]  ## Exists only when `from` is omited in `translate` endpoint
+        ## `detectedLanguage` exists only when `from` is omited in `translate` endpoint
+        detectedLanguage*: Option[DetectedLanguage]
         translations*: seq[BaseTranslation]
 
     Transliteration* = object
@@ -121,8 +134,6 @@ proc toJArray(text: openArray[string]): JsonNode =
     for t in text:
         result.add(JsonNode(kind: JObject, fields: {"Text": %t}.toOrderedTable))
 
-let client = newHttpClient()
-
 proc endpoint(`method`: string, query: seq[(string, string)]): string =
     $(api_base / `method` ? api_version & query)
 
@@ -138,8 +149,8 @@ proc newHeaders(key: string): HttpHeaders =
         "X-ClientTraceId": @[$generateUUID()]
     }.newTable
 
-proc request(uri, key, body: string): Response =
-    result = client.request(uri, httpMethod = HttpPost, headers = newHeaders(key), body = body)
+proc request(client: HttpClient, uri, key, body: string, httpMethod = HttpPost, headers = newHeaders(key)): Response =
+    result = client.request(uri, httpMethod = httpMethod, headers = headers, body = body)
 
     if result.code.is4xx:
         let json = parseJson(result.body)
@@ -148,8 +159,19 @@ proc request(uri, key, body: string): Response =
         else:
             raise (ref AzureTranslateError)(msg: result.body)
 
+proc request(client: AsyncHttpClient, uri, key, body: string, httpMethod = HttpPost, headers = newHeaders(key)): Future[AsyncResponse] {.async.} =
+    result = await client.request(uri, httpMethod = httpMethod, headers = headers, body = body)
 
-proc languages*(key: string, scope: openArray[Scope] = [], acceptLanguage: Option[string] = none(string)): Languages =
+    if result.code.is4xx:
+        let json = parseJson(await result.body)
+        if json.hasKey("error"):
+            raise (ref AzureTranslateError)(code: json["error"]["code"].getInt, msg: json["error"]["message"].getStr)
+        else:
+            raise (ref AzureTranslateError)(msg: await result.body)
+
+
+proc languages*(client: HttpClient|AsyncHttpClient, key: string, 
+    scope: seq[Scope] = @[], acceptLanguage: Option[string] = none(string)): Future[Languages] {.multisync.} =
     ## Gets the set of languages currently supported by other operations of the Translator.
     var headers = newHeaders(key)
     if acceptLanguage.isSome:
@@ -159,17 +181,21 @@ proc languages*(key: string, scope: openArray[Scope] = [], acceptLanguage: Optio
     if scope.len > 0:
         uri &= "&scope=" & scope.join(",")
     
-    let resp = client.request(uri, httpMethod = HttpGet, headers = newHeaders(key))
-    var respJson = parseJson(resp.body)
+    result = parseJson(await body await client.request(uri, "", "", HttpGet, headers)).to Languages
+    result = parseJson(await (await client.request(uri, "", "", HttpGet, headers)).body).to Languages
+    let
+        resp = await client.request(uri, "", "", HttpGet, headers)
+        respJson = parseJson(await resp.body)
 
     result = to(respJson, Languages)
 
-proc translate*(key: string, text: TextQuery, to: openArray[string], `from`: Option[string] = none(string), 
+proc translate*(client: HttpClient|AsyncHttpClient, key: string, 
+    text: TextQuery, to: seq[string], `from`: Option[string] = none(string), 
     textType: Option[TextType] = none(TextType), category: Option[string] = none(string), 
     profanityAction: Option[ProfanityAction] = none(ProfanityAction), profanityMarker: Option[ProfanityMarker] = none(ProfanityMarker),
     includeAlignment, includeSentenceLength: Option[bool] = none(bool),
     suggestedFrom, fromScript, toScript: Option[string] = none(string),
-    allowFallback: Option[bool] = none(bool)): seq[Translation] = 
+    allowFallback: Option[bool] = none(bool)): Future[seq[Translation]] {.multisync.} = 
     ## Translates text.
     ## Note that using category has not been tested
 
@@ -200,32 +226,34 @@ proc translate*(key: string, text: TextQuery, to: openArray[string], `from`: Opt
         query.add({"allowFallback": $allowFallback.get})
 
     let 
-        resp = request(endpoint("translate", query), key, $text.toJArray)
-        respJson = parseJson(resp.body)
+        resp = await client.request(endpoint("translate", query), key, $text.toJArray)
+        respJson = parseJson(await resp.body)
     
     result = toSeq[Translation](respJson)
 
 
-proc transliterate*(key: string, text: TextQuery, language, fromScript, toScript: string): seq[Transliteration] =
-    ## Converts text in one language from one script to another script.
+proc transliterate*(client: HttpClient|AsyncHttpClient, key: string, text: TextQuery, language, fromScript, toScript: string): Future[seq[Transliteration]] {.multisync.} =
+    ## Converts text in one language from one script to another script
+    
     let 
         query = @{"language": language, "fromScript": fromScript, "toScript": toScript}
-        resp = request(endpoint("transliterate", query), key, $text.toJArray)
-        respJson = parseJson(resp.body)
+        resp = await client.request(endpoint("transliterate", query), key, $text.toJArray)
+        respJson = parseJson(await resp.body)
 
     result = toSeq[Transliteration](respJson)
 
 
-proc detect*(key: string, text: TextQuery): seq[Detection] =
+proc detect*(client: HttpClient|AsyncHttpClient, key: string, text: TextQuery): Future[seq[Detection]] {.multisync.} =
     ## Identifies the language of a piece of text
+    
     let 
-        resp = request(endpoint("detect"), key, $text.toJArray)
-        respJson = parseJson(resp.body)
+        resp = await client.request(endpoint("detect"), key, $text.toJArray)
+        respJson = parseJson(await resp.body)
 
     result = toSeq[Detection](respJson)
 
 
-proc break_sentence*(key: string, text: TextQuery, language, script: Option[string] = none(string)): seq[BreakSentence] = 
+proc break_sentence*(client: HttpClient|AsyncHttpClient, key: string, text: TextQuery, language, script: Option[string] = none(string)): Future[seq[BreakSentence]] {.multisync.} = 
     ## Identifies the positioning of sentence boundaries in a piece of text
 
     var uri = endpoint("breaksentence")
@@ -235,28 +263,29 @@ proc break_sentence*(key: string, text: TextQuery, language, script: Option[stri
         uri &= "&script=" & script.get
 
     let 
-        resp = request(uri, key, $text.toJArray)
-        respJson = parseJson(resp.body)
+        resp = await client.request(uri, key, $text.toJArray)
+        respJson = parseJson(await resp.body)
 
     result = toSeq[BreakSentence](respJson)
     
 
-proc dictionary_lookup*(key: string, text: TextQuery, `from`, to: string): seq[DictionaryLookup] =
+proc dictionary_lookup*(client: HttpClient|AsyncHttpClient, key: string, text: TextQuery, `from`, to: string): Future[seq[DictionaryLookup]] {.multisync.} =
     ## Provides alternative translations for a word and a small number of idiomatic phrases. 
     ## Each translation has a part-of-speech and a list of back-translations. 
     ## The back-translations enable a user to understand the translation in context. 
     ## The Dictionary Example operation allows further drill down to see example uses of each translation pair.
+    
     let 
         uri = endpoint("dictionary/lookup", @{"from": `from`, "to": to})
-        resp = request(uri, key, $text.toJArray)
-        respJson = parseJson(resp.body)
+        resp = await client.request(uri, key, $text.toJArray)
+        respJson = parseJson(await resp.body)
 
     result = toSeq[DictionaryLookup](respJson)
 
 
-proc dictionary_examples*(key: string, query: openArray[DictExQuery], `from`, to: string): seq[DictionaryExample] =
-    ## Provides examples that show how terms in the dictionary are used in context. 
-    ## This operation is used in tandem with Dictionary lookup.
+proc dictionary_examples*(client: HttpClient|AsyncHttpClient, key: string, query: seq[DictExQuery], `from`, to: string): Future[seq[DictionaryExample]] {.multisync.} =
+    ## Provides examples that show how terms in the dictionary are used in context
+    ## This operation is used in tandem with Dictionary lookup
     
     var body = JsonNode(kind: JArray, elems: @[])
     for t in query:
@@ -264,7 +293,7 @@ proc dictionary_examples*(key: string, query: openArray[DictExQuery], `from`, to
 
     let 
         uri = endpoint("dictionary/examples", @{"from": `from`, "to": to})
-        resp = request(uri, key, $body)
-        respJson = parseJson(resp.body)
+        resp = await client.request(uri, key, $body)
+        respJson = parseJson(await resp.body)
 
     result = toSeq[DictionaryExample](respJson)
--- tests/test_async.nim
+++ tests/test_async.nim
@@ -5,45 +5,45 @@
 #
 # To run these tests, simply execute `nimble test`.
 
-import unittest, options, os, tables, strutils, re, httpclient
+import unittest, options, os, tables, strutils, re, httpclient, asyncdispatch
 
 import azure_translate
 
 let 
-    client = newHttpClient()
+    client = newAsyncHttpClient()
     key = getEnv("AZURE_SECRET_KEY")
 
-test "invalid key":
+test "async invalid key":
     expect AzureTranslateError:
-        discard translate(client, "", @[""], @[""])
+        discard waitFor translate(client, "", @[""], @[""])
 
-suite "languages":
-    let all = languages(client, key)
+suite "async languages":
+    let all = waitFor languages(client, key)
 
-    test "translation languages":
+    test "async translation languages":
         check(all.translation.isSome)
         check(all.translation.get["en"] == TranslationLanguage(dir: "ltr", name: "English", nativeName: "English"))
     
-    test "dictionary languages":
+    test "async dictionary languages":
         check(all.dictionary.isSome)
         check(all.dictionary.get.hasKey("en"))
 
-    test "transliteration languages":
+    test "async transliteration languages":
         check(all.transliteration.isSome)
         check(all.transliteration.get.hasKey("ar"))
     
-    test "single language":
+    test "async single language":
         ## Ensures that only one of the 3 language types are populated
-        let single = languages(client, key, @[azDictionary])
+        let single = waitFor languages(client, key, @[azDictionary])
         check(single.dictionary.isSome)
         check(single.dictionary.get.hasKey("en"))
 
         check(single.translation.isNone)
         check(single.transliteration.isNone)
 
-    test "multiple languages":
+    test "async multiple languages":
         ## Ensures that only two of the 3 language types are populated
-        let multiple = languages(client, key, @[azTransliteration, azTranslation])
+        let multiple = waitFor languages(client, key, @[azTransliteration, azTranslation])
         check(multiple.dictionary.isNone)
         
         check(multiple.translation.isSome)
@@ -51,30 +51,30 @@ suite "languages":
         check(multiple.transliteration.isSome)
         check(all.transliteration.get.hasKey("ar"))
 
-suite "translate":
+suite "async translate":
     const en_text = @["Hello, what is your name?"]
 
-    test "single input":
-        let translation = translate(client, key, en_text, to = @["zh-Hans"], `from` = some("en"))[0].translations[0]
+    test "async single input":
+        let translation = translate(client, key, en_text, to = @["zh-Hans"], `from` = some("en")).waitFor[0].translations[0]
 
         check(translation.to == "zh-Hans")
         check(not translation.text.isEmptyOrWhitespace)
 
-    test "single input auto-detection":
-        let translation = translate(client, key, en_text, @["es"])[0]
+    test "async single input auto-detection":
+        let translation = translate(client, key, en_text, @["es"]).waitFor[0]
         check(translation.detectedLanguage.get.language == "en")
         check(translation.translations[0].to == "es")
         check(not translation.translations[0].text.isEmptyOrWhitespace)
 
-    test "transliteration":
-        let translation = translate(client, key, en_text, @["zh-Hans"], toScript=some("Latn"))[0]
+    test "async transliteration":
+        let translation = translate(client, key, en_text, @["zh-Hans"], toScript=some("Latn")).waitFor[0]
 
         check(translation.detectedLanguage.get.language == "en")
         check(translation.translations[0].to == "zh-Hans")
         check(not translation.translations[0].text.isEmptyOrWhitespace)
 
-    test "multiple inputs":
-        let translations = translate(client, key, text = en_text & @["I am fine, thank you."], to = @["zh-Hans"], `from` = some("en"))
+    test "async multiple inputs":
+        let translations = waitFor translate(client, key, text = en_text & @["I am fine, thank you."], to = @["zh-Hans"], `from` = some("en"))
 
         let first = translations[0].translations[0]
         check(first.to == "zh-Hans")
@@ -84,8 +84,8 @@ suite "translate":
         check(second.to == "zh-Hans")
         check(not second.text.isEmptyOrWhitespace)
 
-    test "multiple languages":
-        let translations = translate(client, key, en_text, to = @["zh-Hans", "de"], `from` = some("en"))[0].translations
+    test "async multiple languages":
+        let translations = translate(client, key, en_text, to = @["zh-Hans", "de"], `from` = some("en")).waitFor[0].translations
 
         let zh_hans = translations[0]
         check(zh_hans.to == "zh-Hans")
@@ -95,41 +95,41 @@ suite "translate":
         check(de.to == "de")
         check(not de.text.isEmptyOrWhitespace)
 
-    test "profanity":
-        let asterisk = translate(client, key, @["This is a fucking good idea."], to = @["de"], `from` = some("en"), profanityAction=some(ProfanityAction.marked))[0]
+    test "async profanity":
+        let asterisk = translate(client, key, @["This is a fucking good idea."], to = @["de"], `from` = some("en"), profanityAction=some(ProfanityAction.marked)).waitFor[0]
             .translations[0]
         
         check(asterisk.to == "de")
         check("***" in asterisk.text)
 
-        let tag = translate(client, key, @["This is a fucking good idea."], to = @["de"], `from` = some("en"), profanityAction=some(ProfanityAction.marked), profanityMarker=some(ProfanityMarker.tag))[0]
+        let tag = translate(client, key, @["This is a fucking good idea."], to = @["de"], `from` = some("en"), profanityAction=some(ProfanityAction.marked), profanityMarker=some(ProfanityMarker.tag)).waitFor[0]
             .translations[0]
 
         check(tag.to == "de")
         check(contains(tag.text, re"<profanity>[\s\S]+</profanity>"))
 
 
-    test "notranslate":
+    test "async notranslate":
         ## translate content with markup and decide what's translated
         const content_markup_text = @["""<div class="notranslate">This will not be translated.</div>
         <div>This will be translated. </div>"""]
-        let translation = translate(client, key, content_markup_text, to = @["zh-Hans"], `from` = some("en"), textType = some(TextType.html))[0]
+        let translation = translate(client, key, content_markup_text, to = @["zh-Hans"], `from` = some("en"), textType = some(TextType.html)).waitFor[0]
             .translations[0]
 
         check(translation.to == "zh-Hans")
         check("<div class=\"notranslate\">This will not be translated.</div>" in translation.text)
 
-    test "alignment":
-        let translation = translate(client, key, @["The answer lies in machine translation."], to = @["fr"], some("en"), includeAlignment=some(true))[0]
+    test "async alignment":
+        let translation = translate(client, key, @["The answer lies in machine translation."], to = @["fr"], some("en"), includeAlignment=some(true)).waitFor[0]
             .translations[0]
 
         check(translation.to == "fr")
         check(translation.alignment.isSome)
         check(not translation.alignment.get.proj.isEmptyOrWhitespace)
 
-    test "sentence boundaries":
+    test "async sentence boundaries":
         const sentence_boundary_text = @["The answer lies in machine translation. The best machine translation technology cannot always provide translations tailored to a site or users like a human. Simply copy and paste a code snippet anywhere."]
-        let translation = translate(client, key, sentence_boundary_text, @["fr"], some("en"), includeSentenceLength=some(true))[0]
+        let translation = translate(client, key, sentence_boundary_text, to = @["fr"], some("en"), includeSentenceLength=some(true)).waitFor[0]
             .translations[0]
         
         check(translation.to == "fr")
@@ -137,35 +137,35 @@ suite "translate":
         check(translation.sentLen.get.srcSentLen == @[Natural(40), 117, 46])
         check(translation.sentLEn.get.transSentLen.len == 3)    # Only checking length as the numbers could change
 
-    test "dynamic dictionary":
+    test "async dynamic dictionary":
         const dynamic_dictionary_text = @["The word <mstrans:dictionary translation=\"wordomatic\">word or phrase</mstrans:dictionary> is a dictionary entry."]
-        let translation = translate(client, key, dynamic_dictionary_text, @["de"], some("en"))[0]
+        let translation = translate(client, key, dynamic_dictionary_text, @["de"], some("en")).waitFor[0]
             .translations[0]
         check(translation.to == "de")
         check("wordomatic" in translation.text)
 
-test "transliterate":
-    let res = transliterate(client, key, @["こんにちは", "さようなら"], "ja", "Jpan", "Latn")
+test "async transliterate":
+    let res = waitFor transliterate(client, key, @["こんにちは", "さようなら"], "ja", "Jpan", "Latn")
     check(res[0].script == "Latn" and not res[0].text.isEmptyOrWhitespace)
     check(res[1].script == "Latn" and not res[1].text.isEmptyOrWhitespace)
 
-test "detect":
-    let res = detect(client, key, @["What language is this text written in?", "이 텍스트는 어떤 언어로 쓰여져 있습니까?"])
+test "async detect":
+    let res = waitFor detect(client, key, @["What language is this text written in?", "이 텍스트는 어떤 언어로 쓰여져 있습니까?"])
     check(res[0].language == "en")
     check(res[1].language == "ko")
 
-test "break setence":
-    let res = break_sentence(client, key, @["How are you? I am fine. What did you do today?"], language=some("en"))
+test "async break setence":
+    let res = waitFor break_sentence(client, key, @["How are you? I am fine. What did you do today?"], language=some("en"))
     check(res[0].sentLen == @[Natural(13), 11, 22])  # Compiler assumes int if Natural is missing
 
-test "dictionary lookup":
-    let entry = dictionary_lookup(client, key, @["fly"], `from`="en", to="es")[0]  # Getting first! (may need to change in the future)
+test "async dictionary lookup":
+    let entry = dictionary_lookup(client, key, @["fly"], `from`="en", to="es").waitFor[0]  # Getting first! (may need to change in the future)
     check(entry.normalizedSource == "fly")
     check(entry.displaySource == "fly")
     check(entry.translations.len > 0)
 
-test "dictionary examples":
-    let example = dictionary_examples(client, key, @[(text: "fly", translation: "volar")], `from`="en", to="es")[0]
+test "async dictionary examples":
+    let example = dictionary_examples(client, key, @[(text: "fly", translation: "volar")], `from`="en", to="es").waitFor[0]
     check(example.normalizedSource == "fly")
     check(example.normalizedTarget == "volar")
     check(example.examples.len > 0)
